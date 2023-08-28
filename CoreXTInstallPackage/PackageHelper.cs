46f9265e8ca6c18f5884066ae2013a4050d50b53
winter2048 <522112669@qq.com>
2023-08-29 00:11:10 +0800

    update

---
+++ CoreXTInstallPackage/PackageHelper.cs
@@ -0,0 +1,78 @@
+﻿using NuGet.Common;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreXTInstallPackage
+{
+    public static class PackageHelper
+    {
+        static ILogger logger = NullLogger.Instance;
+        static CancellationToken cancellationToken = CancellationToken.None;
+        static SourceCacheContext cache = new SourceCacheContext();
+        static SourceRepository repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+
+        static List<string> net48 = new List<string>{ ".NETStandard,Version=v2.1", ".NETStandard,Version=v2.0" };
+
+        public static async Task<List<PackageDependency>> GetDependencys(string packageId, string? version = null)
+        {
+            FindPackageByIdResource resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+            NuGetVersion nuGetVersion = null;
+            if (version != null)
+            {
+                nuGetVersion = NuGetVersion.Parse(version);
+            }
+            else
+            {
+                IEnumerable<NuGetVersion> versions = await resource.GetAllVersionsAsync(
+                packageId,
+                cache,
+                logger,
+                cancellationToken);
+                nuGetVersion = versions.Last();
+            }
+
+            var dependencys = await resource.GetDependencyInfoAsync(packageId,
+                 nuGetVersion,
+                 cache,
+                 logger,
+                 cancellationToken);
+
+            var list = new List<PackageDependency>();
+            var list2 = dependencys.DependencyGroups.Where(p => net48.Contains(p.TargetFramework.DotNetFrameworkName)).ToList();
+
+            if (list2 == null || list2.Count == 0)
+            {
+                return list;
+            }
+            else
+            {
+                return list2[0].Packages.ToList();
+            }
+        }
+
+        public static async Task<List<PackageDependency>> GetDependencysAll(string packageId, string? version = null)
+        {
+            List<PackageDependency> list = new List<PackageDependency>();
+           //await Task.Delay(2000);
+            var des = await PackageHelper.GetDependencys(packageId, version);
+            foreach (var item in des)
+            {
+                list.Add(item);
+                var ss = await PackageHelper.GetDependencysAll(item.Id, item.VersionRange.OriginalString);
+                if (ss.Count > 0)
+                {
+                    list.AddRange(ss);
+                }
+            }
+            return list.GroupBy(p => p.Id).Select(p => p.OrderByDescending(p => p.VersionRange.MinVersion).FirstOrDefault()).ToList();
+            //return list;
+        }
+    }
+}
---
+++ CoreXTInstallPackage/Program.cs
@@ -0,0 +1,24 @@
+﻿using NuGet.Common;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace CoreXTInstallPackage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+             
+              var list =  await PackageHelper.GetDependencysAll("Azure.Identity");
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"Found dependency {item.Id}  {item.VersionRange}");
+                }
+                //Console.ReadKey();
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
