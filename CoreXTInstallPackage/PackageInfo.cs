d8d8c8b384c45d8143bb51f1175cacef6f0fccc2
Haitao Jin <v-haitaojin@microsoft.com>
2023-08-29 17:34:56 +0800

    update

---
+++ CoreXTInstallPackage/PackageInfo.cs
@@ -0,0 +1,19 @@
+﻿using NuGet.Frameworks;
+using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreXTInstallPackage
+{
+	public class PackageInfo
+	{
+        public string Id { get; set; }
+
+        public VersionRange Version { get; set; }
+
+        public NuGetFramework? Tfm { get; set; }
+    }
+}
